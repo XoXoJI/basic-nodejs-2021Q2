b24351a96b24edd2b4de29ce3abcdad078088dec
Petrachkov A.D <xoxoji19910501@gmail.com>
2021-05-16 01:05:20 +0300

    refactor: add lib crudController

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -1,84 +1,43 @@
 const router = require('express').Router();
-const EntityNotExistsError = require('../../lib/error/dbError/entityNotExistsError');
+const CRUDController = require('../../lib/controller/crudController');
 const Board = require('./board.model');
 const boardService = require('./board.service');
+
+const crudController = new CRUDController(boardService, Board.toResponse);
 
 /**
  * Получение всех досок
  */
 router.route('/').get(async (req, res) => {
-    const users = await boardService.getAll();
-
-    res.json(users.map(Board.toResponse));
+    await crudController.getAll(res);
 });
 
 /**
  * Получение доски по id
  */
 router.route('/:boardId').get(async (req, res) => {
-    const board = await boardService.get(req.params.boardId);
-
-    if(!board) {
-        res.sendStatus(404);
-    }
-    else {
-        res.json(Board.toResponse(board));
-    }
+    await crudController.get(req.params.boardId, res);
 });
 
 /**
  * Создание доски
  */
 router.route('/').post(async (req, res) => {
-    try {
-        const board = await boardService.create(req.body);
-
-        res.status(201).json(Board.toResponse(board));
-    }
-    catch(err) {
-        console.error(err.message);
-        res.sendStatus(500);
-    }
+    await crudController.create(req.body, res);
 });
 
 /**
  * Обновление данных доски
  */
 router.route('/:boardId').put(async (req, res) => {
-    try {
-        const board = await boardService.update(
-            Object.assign(
-                { id: req.params.boardId },
-                req.body
-            )
-        );
-
-        res.json(Board.toResponse(board));
-    }
-    catch(err) {
-        console.error(err.message);
-        res.sendStatus(500);
-    }
+    await crudController.update(req.params.boardId, req.body, res);
 });
 
 /**
  * Удаление доски
  */
 router.route('/:boardId').delete(async (req, res) => {
-    try {
-        await boardService.delete(req.params.boardId);
-
-        res.sendStatus(200);
-    }
-    catch(err) {
-        console.error(err.message);
-
-        if(err instanceof EntityNotExistsError) {
-            res.sendStatus(204);
-        } else {
-            res.sendStatus(500);
-        }
-    }
+    await crudController.delete(req.params.boardId, res);
 });
 
 module.exports = router;
--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -1,83 +1,43 @@
 const router = require('express').Router();
-const EntityNotExistsError = require('../../lib/error/dbError/entityNotExistsError');
+const CRUDController = require('../../lib/controller/crudController');
 const User = require('./user.model');
 const usersService = require('./user.service');
+
+const crudController = new CRUDController(usersService, User.toResponse);
 
 /**
  * Получение всех пользователей
  */
 router.route('/').get(async (req, res) => {
-    const users = await usersService.getAll();
-
-    res.json(users.map(User.toResponse));
+    await crudController.getAll(res);
 });
 
 /**
  * Получение пользователя по id
  */
 router.route('/:userId').get(async (req, res) => {
-    const user = await usersService.get(req.params.userId);
-
-    if (!user) {
-        res.sendStatus(404);
-    } else {
-        res.json(User.toResponse(user));
-    }
+    await crudController.get(req.params.userId, res);
 });
 
 /**
  * Создание пользователя
  */
 router.route('/').post(async (req, res) => {
-    try {
-        const user = await usersService.create(req.body);
-
-        res.status(201).json(User.toResponse(user));
-    }
-    catch(err) {
-        console.error(err.message);
-        res.sendStatus(500);
-    }
+    await crudController.create(req.body, res);
 });
 
 /**
  * Обновление данных пользователя
  */
 router.route('/:userId').put(async (req, res) => {
-    try {
-        const user = await usersService.update(
-            Object.assign(
-                { id: req.params.userId },
-                req.body
-            )
-        );
-
-        res.json(User.toResponse(user));
-    }
-    catch(err) {
-        console.error(err.message);
-        res.sendStatus(500);
-    }
+    await crudController.update(req.params.userId, req.body, res);
 });
 
 /**
  * Удаление пользователя
  */
 router.route('/:userId').delete(async (req, res) => {
-    try {
-        await usersService.delete(req.params.userId);
-
-        res.sendStatus(200);
-    }
-    catch(err) {
-        console.error(err.message);
-
-        if(err instanceof EntityNotExistsError) {
-            res.sendStatus(204);
-        } else {
-            res.sendStatus(500);
-        }
-    }
+    await crudController.delete(req.params.userId, res);
 });
 
 module.exports = router;

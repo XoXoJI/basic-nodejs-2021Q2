6b2cc6a81c29d7981eda687b76f274690c7b3366
Petrachkov A.D <xoxoji19910501@gmail.com>
2021-05-23 16:52:24 +0300

    refactor: add comments to task.service

--- src/resources/tasks/task.service.js
+++ src/resources/tasks/task.service.js
@@ -3,24 +3,44 @@ const TaskRepository = require("./task.memory.repository");
 const CRUDService = require("../../lib/service/crudService");
 
 class UserService extends CRUDService {
+    /**
+     * @constructor
+     */
     constructor() {
         super();
 
         this.repository = new TaskRepository(db);
     }
 
+    /**
+     * Get all tasks
+     * @param {string} idBoard - id board
+     * @returns {Promise<import("../model")[]>} tasks
+     */
     async getAll(idBoard) {
         const tasks = await this.repository.getAll(idBoard);
 
         return tasks;
     }
 
+    /**
+     * Get task from id
+     * @param {string} idBoard - id board
+     * @param {string} id - id task
+     * @returns {Promise<import("../model")>} task
+     */
     async get(idBoard, id) {
         const tasks = await this.getAll(idBoard);
 
         return tasks.find((task) => task.id === id);
     }
 
+    /**
+     * Delete task
+     * @param {string} idBoard - id board
+     * @param {string} id - id task
+     * @returns {Promise<void>}
+     */
     async delete(idBoard, id) {
         const tasks = await this.getAll(idBoard);
         const turgetTask = tasks.find((task) => task.id === id);

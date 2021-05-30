93bb2cf9b80d06e64fa2ce2516b3571307d76983
Petrachkov A.D <xoxoji19910501@gmail.com>
2021-05-30 10:55:17 +0300

    feat: include ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import express from 'express';
 import swaggerUI from 'swagger-ui-express';
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { join } from 'path';
 import YAML from 'yamljs';
 import userRouter from './resources/users/user.router.js';
 import boardRouter from './resources/boards/board.router.js';
 import taskRouter from './resources/tasks/task.router.js';
 import { boardService } from './resources/boards/board.service.js';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const swaggerDocument = YAML.load(join(__dirname, '../doc/api.yaml'));
 
@@ -30,6 +28,7 @@ app.param('boardId', async (req, res, next, boardId) => {
     if(!board) {
         res.sendStatus(404);
     } else {
+        //@ts-ignore
         req.board = board;
 
         if(req.body) {
---
+++ src/common/config.ts
@@ -0,0 +1,14 @@
+import { config } from 'dotenv';
+import { join } from 'path';
+
+config({
+    path: join(__dirname, '../../.env')
+});
+
+export const {
+    PORT,
+    NODE_ENV,
+    MONGO_CONNECTION_STRING,
+    JWT_SECRET_KEY,
+    AUTH_MODE,
+} = process.env;

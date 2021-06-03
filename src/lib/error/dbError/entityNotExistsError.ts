4498ace3b7c6e710e8051ab3544187563364ffa3
Petrachkov A.D <xoxoji19910501@gmail.com>
2021-06-03 22:32:42 +0300

    refactor: change last js file to ts

--- src/lib/error/dbError/entityNotExistsError.ts
+++ src/lib/error/dbError/entityNotExistsError.ts
@@ -5,7 +5,7 @@ export default class EntityNotExistsError extends DBError {
      * @constructor
      * @param {string} message - error message
      */
-    constructor(message) {
+    constructor(message: string) {
         super(message);
         this.name = 'EntityNotExistsError';
     }
--- src/lib/error/dbError/idNotUniqueError.ts
+++ src/lib/error/dbError/idNotUniqueError.ts
@@ -5,7 +5,7 @@ export default class IdNotUniqueError extends DBError {
      * @constructor
      * @param {string} message - error message
      */
-    constructor(message) {
+    constructor(message: string) {
         super(message);
         this.name = 'IdNotUniqueError';
     }

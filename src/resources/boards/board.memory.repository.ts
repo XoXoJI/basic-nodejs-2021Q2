36cd4b9572b7c3ff46078e9002007c77078a86c9
Petrachkov A.D <xoxoji19910501@gmail.com>
2021-06-20 21:26:39 +0300

    feat: board get with columns

--- src/resources/boards/board.memory.repository.ts
+++ src/resources/boards/board.memory.repository.ts
@@ -1,8 +1,16 @@
+import { getRepository } from "typeorm";
 import Board from "../../entity/board";
 import CRUDRepository from "../../lib/repository/crudRepository";
 
 export default class BoardRepository extends CRUDRepository<Board> {
     constructor() {
         super(Board);
+    }
+
+    async get(id: string) {
+        return await getRepository(this.entity).findOneOrFail({
+            relations: ["columns"],
+            where: { id },
+        });
     }
 }
